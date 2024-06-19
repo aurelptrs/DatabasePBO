6dedfe8da29d9b40590c67b10a95464539726d60
aurelptrs <119091509+aurelptrs@users.noreply.github.com>
2024-06-19 23:07:54 +0700

    Update main.java

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -6,7 +6,9 @@ public class main {
         int inp;
         
         do {
+            System.out.print.ln("--------------");
             System.out.println("Pilih Menu :");
+            System.out.print.ln("--------------");
             System.out.println("1. Tampilkan");
             System.out.println("2. Masukkan");
             System.out.println("3. Edit");
@@ -19,34 +21,34 @@ public class main {
                     database.tampilkan();
                     break;
                 case 2:
-                    System.out.println("Masukkan Id Buku");
+                    System.out.println("Masukkan ID Buku");
                     int id = input.nextInt();
                     System.out.println("Masukkan Judul Buku");
                     String judul = input.next();
                     System.out.println("Masukkan Tahun Terbit Buku");
                     int tahun = input.nextInt();
-                    System.out.println("Masukkan Jumlah Stok");
+                    System.out.println("Masukkan Jumlah Stok Buku");
                     int stok = input.nextInt();
-                    System.out.println("Masukkan Jumlah Penulis");
+                    System.out.println("Masukkan Jumlah Penulis Buku");
                     int penulis = input.nextInt();
                     database.masukkanBuku(id, judul, tahun, stok, penulis);
                     break;
                 case 3:
-                    System.out.println("Masukkan Id Buku");
+                    System.out.println("Masukkan ID Buku");
                     id = input.nextInt();
                     System.out.println("Masukkan Judul Buku");
                     String Ejudul = input.next();
                     System.out.println("Masukkan Tahun Terbit Buku");
                     tahun = input.nextInt();
-                    System.out.println("Masukkan Jumlah Stok");
+                    System.out.println("Masukkan Jumlah Stok Buku");
                     stok = input.nextInt();
-                    System.out.println("Masukkan Jumlah Penulis");
+                    System.out.println("Masukkan Jumlah Penulis Buku");
                     penulis = input.nextInt();
 
                     database.editBuku(id, Ejudul, tahun, stok, penulis);
                     break;
                 case 4:
-                    System.out.println("Masukkan Id Buku");
+                    System.out.println("Masukkan ID Buku");
                     id = input.nextInt();
                     database.deleteData(id);
                     break;
